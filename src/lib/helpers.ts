c9265c34f8975c08a7acbee1f58c7a072caa9c60
Sebastian Petersen <sebkirller@gmail.com>
2022-02-17 22:06:33 +0100

    Add helper function to calculate contrast

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,4 +2,14 @@ function extractHexColors(text: string) {
     return text.match(/#(?:[0-9a-fA-F]{3}){1,2}/g) ?? [];
 }
 
-export { extractHexColors }
+// https://stackoverflow.com/questions/11867545
+function getContrastYIQ(hexcolor: String){
+    hexcolor = hexcolor.replace("#", "");
+    var r = parseInt(hexcolor.substring(0,2),16);
+    var g = parseInt(hexcolor.substring(2,4),16);
+    var b = parseInt(hexcolor.substring(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? 'black' : 'white';
+}
+
+export { extractHexColors, getContrastYIQ }
